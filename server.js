7daa017e54bb08c2f9443c92eb321ce1d05db73f
unknown <chamath.rmc@gmail.com>
2020-02-09 08:42:44 +0530

    nodeapp

---
+++ server.js
@@ -0,0 +1,22 @@
+/*const express = require('express');
+const path = require('path');
+
+//init app
+const app = express();
+
+ //load view engine
+ app.set('views' , path.join(__dirname, 'views'));
+ app.set('view engine' , 'pug');
+
+  //home route
+  app.get('/' , function(req, res){
+    res.render('index' ,{
+
+    });
+
+  });
+
+  //server
+  app.listen(5000, function(){
+    console.log('started');
+  });*/
